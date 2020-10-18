2b3b0cb60b24ebe95b7bb7bd63a3d2166a656b0b
Glavo <zjx001202@126.com>
2020-10-18 23:42:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+## Kala Collection
+
+[![Build Status](https://travis-ci.com/Glavo/kala-collection.svg?branch=master)](https://travis-ci.com/Glavo/kala-collection)
+[ ![Download](https://api.bintray.com/packages/glavo/maven/kala-collection/images/download.svg) ](https://bintray.com/glavo/maven/kala-collection/_latestVersion)
+
+
---
+++ src/main/java/asia/kala/collection/Map.java
@@ -0,0 +1,21 @@
+package asia.kala.collection;
+
+import asia.kala.function.CheckedBiConsumer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.BiConsumer;
+
+public interface Map<K, V> {
+
+    void forEach(@NotNull BiConsumer<? super K, ? super V> consumer);
+
+    default <Ex extends Throwable> void forEachChecked(
+            @NotNull CheckedBiConsumer<? super K, ? super V, ? extends Ex> consumer
+    ) throws Ex {
+        forEach(consumer);
+    }
+
+    default void forEachUnchecked(@NotNull CheckedBiConsumer<? super K, ? super V, ?> consumer) {
+        forEach(consumer);
+    }
+}
